215c7274483de29d2715cf6a99f9d5224de2efee
Oros42 <oros@ecirtam.net>
2013-10-23 23:41:03 +0200

    fix parseur

--- bridge/donmai.php
+++ bridge/donmai.php
@@ -11,80 +11,89 @@ if(isset($_GET['update']) && !isset($_POST['init_booru'])){
 		$dom = new DOMDocument();
 		if($dom->load($site_rss)){
 			$flux_rss = $dom->getElementsByTagName('entry');
+			if($flux_rss->length > 0){
+				require_once __DIR__.'/../database.php';
+				$db=new Database($booru_dir);
 
-			require_once __DIR__.'/../database.php';
-			$db=new Database($booru_dir);
-
-			$last_insert=$db->get_last_insert();
-			if(!empty($last_insert)){
-				$start=0;
-				// search if last insert image is in list
-				for ($start=0; $start < $flux_rss->length ; $start++) { 
-					if($last_insert['page_url']==$flux_rss->item($start)->getElementsByTagName('a')->item(0)->getAttribute('href')){
-						$start--;
-						break;
-					}
-				}
-			}else{
-				$start=$flux_rss->length-1;
-			}
-			for($i=$start; $i>2;$i--){
-				$item=array();
-				$item['page_url']=$flux_rss->item($i)->getElementsByTagName('a')->item(0)->getAttribute('href');
-				$item['file_id']=substr($item['page_url'], strrpos($item['page_url'], '/')+1);
-				$dom2 = new DOMDocument();
-				if(@$dom2->loadHTMLFile($item['page_url'], LIBXML_ERR_NONE)){ // get page
-					if($dom2->getElementById('image')){ // if not deleted
-						$data = $dom2->getElementById('image-resize-link');
-						if(!empty($data)){
-							$data = $data->getAttribute('href');
-						}else{
-							$data = $dom2->getElementById('image')->getAttribute('src');	
-						}					
-						if(!empty($data)){
-							// download image
-							$data=str_replace('sample/sample-', '', $data);
-							$item['img_url']=$site.substr($data, 1);
-							if(strrpos($data, "?")>0){
-								$item['img_name']=htmlentities(substr($data, strrpos($data, "/")+1, - (strlen($data)-strrpos($data, "?"))));
-							}else{
-								$item['img_name']=htmlentities(substr($data, strrpos($data, "/")+1));
-							}
-							file_put_contents(__DIR__.'/../mirror/'.$booru_dir.'/img/'.$item['img_name'], file_get_contents($item['img_url']));
+				$last_insert=$db->get_last_insert();
+				if(!empty($last_insert)){
+					$start=0;
+					// search if last insert image is in list
+					for ($start=0; $start < $flux_rss->length ; $start++) { 
+						if($last_insert['page_url']==$flux_rss->item($start)->getElementsByTagName('a')->item(0)->getAttribute('href')){
+							break;
 						}
-						// get stats
-						$data = $dom2->getElementById('sidebar')->childNodes->item(4)->getElementsByTagName('li');
-						if(!empty($data)){
-							$item['posted']=substr($data->item(2)->getElementsByTagName('time')->item(0)->getAttribute('title'), 0, -6);
-							$item['author']=$data->item(1)->getElementsByTagName('a')->item(0)->nodeValue;
-							$item['author_url']=$site.substr($data->item(1)->getElementsByTagName('a')->item(0)->getAttribute('href'), 1);
-							$a=explode(" ", trim($data->item(3)->nodeValue));
-							$a=explode('x',substr($a[count($a)-1], 1,-1));
-							$item['width']=$a[0];
-							$item['height']=$a[1];
-							$item['source']=$data->item(4)->getElementsByTagName('a')->item(0)->getAttribute('href');
-							$item['rating']=substr($data->item(5)->nodeValue, 8);
-							$item['score']=$data->item(7)->getElementsByTagName('span')->item(0)->nodeValue;
-							$item['tags']='';
-							$data = $dom2->getElementById('tag-list')->childNodes->item(5)->getElementsByTagName('li');
+					}
+					$start--;
+				}else{
+					$start=$flux_rss->length-1;
+				}
+				for($i=$start; $i>=0;$i--){
+					$item=array();
+					$item['page_url']=$flux_rss->item($i)->getElementsByTagName('a')->item(0)->getAttribute('href'); // FIXME some time : error Call to a member function getElementsByTagName()
+					$item['file_id']=substr($item['page_url'], strrpos($item['page_url'], '/')+1);
+					$dom2 = new DOMDocument();
+					if(@$dom2->loadHTMLFile($item['page_url'], LIBXML_ERR_NONE)){ // get page
+						if($dom2->getElementById('image')){ // if not deleted
+							$data = $dom2->getElementById('image-resize-link');
 							if(!empty($data)){
-								foreach ($data as $tag) {
-									$a=$tag->getElementsByTagName('a');
-									if(!empty($a) && $a->length >0){
-										$item['tags'].=$a->item(1)->nodeValue.";";
+								$data = $data->getAttribute('href');
+							}else{
+								$data = $dom2->getElementById('image')->getAttribute('src');	
+							}					
+							if(!empty($data)){
+								// download image
+								$data=str_replace('sample/sample-', '', $data);
+								$item['img_url']=$site.substr($data, 1);
+								if(strrpos($data, "?")>0){
+									$item['img_name']=htmlentities(substr($data, strrpos($data, "/")+1, - (strlen($data)-strrpos($data, "?"))));
+								}else{
+									$item['img_name']=htmlentities(substr($data, strrpos($data, "/")+1));
+								}
+								file_put_contents(__DIR__.'/../mirror/'.$booru_dir.'/img/'.$item['img_name'], file_get_contents($item['img_url']));
+							}
+							// get stats
+							$data = $dom2->getElementById('sidebar')->childNodes->item(4)->getElementsByTagName('li');
+							if(!empty($data)){
+								$item['posted']=substr($data->item(2)->getElementsByTagName('time')->item(0)->getAttribute('title'), 0, -6);
+								$item['author']=$data->item(1)->getElementsByTagName('a')->item(0)->nodeValue;
+								$item['author_url']=$site.substr($data->item(1)->getElementsByTagName('a')->item(0)->getAttribute('href'), 1);
+								$a=explode(" ", trim($data->item(3)->nodeValue));
+								$a=explode('x',substr($a[count($a)-1], 1,-1));
+								if(count($a)==2){
+									$item['width']=$a[0];
+									$item['height']=$a[1];
+								}
+								if($data->item(4)->getElementsByTagName('a')->length>0){
+									$item['source']=$data->item(4)->getElementsByTagName('a')->item(0)->getAttribute('href');
+								}else{
+									$item['source']=substr($data->item(4)->nodeValue, 8);
+								}
+								$item['rating']=substr($data->item(5)->nodeValue, 8);
+								$item['score']=$data->item(7)->getElementsByTagName('span')->item(0)->nodeValue;
+								$item['tags']='';
+								$data = $dom2->getElementById('tag-list')->childNodes->item($dom2->getElementById('tag-list')->childNodes->length-1)->getElementsByTagName('li');
+								if(!empty($data)){
+									foreach ($data as $tag) {
+										$a=$tag->getElementsByTagName('a');
+										if(!empty($a) && $a->length >0){
+											$item['tags'].=$a->item(1)->nodeValue.";";
+										}
 									}
 								}
 							}
 						}
 					}
+					$db->insert($item);
 				}
-				$db->insert($item);
-			}
-			if($start>2){
-				echo 'Update done';
+				if($start>=0){
+					echo 'Update done';
+				}else{
+					echo 'No news';
+				}
 			}else{
-				echo 'No news';
-			}			
+				echo 'error';
+			}
 		}
 	}
 }
--- bridge/gelbooru.php
+++ bridge/gelbooru.php
@@ -11,83 +11,86 @@ if(isset($_GET['update']) && !isset($_POST['init_booru'])){
 		$dom = new DOMDocument();
 		if($dom->load($site_rss)){
 			$flux_rss = $dom->getElementsByTagName('link');
+			if($flux_rss->length > 0){
+				require_once __DIR__.'/../database.php';
+				$db=new Database($booru_dir);
 
-			require_once __DIR__.'/../database.php';
-			$db=new Database($booru_dir);
-
-			$last_insert=$db->get_last_insert();
-			if(!empty($last_insert)){
-				$start=0;
-				// search if last insert image is in list
-				for ($start=0; $start < $flux_rss->length ; $start++) { 
-					if($last_insert['page_url']==$flux_rss->item($start)->nodeValue){
-						$start--;
-						break;
-					}
-				}
-			}else{
-				$start=$flux_rss->length-1;
-			}
-			for($i=$start; $i>2;$i--){
-				$item=array();
-				$item['page_url']=$flux_rss->item($i)->nodeValue;
-				$item['file_id']=substr($item['page_url'], strrpos($item['page_url'], '=')+1);
-				$dom2 = new DOMDocument();
-				if(@$dom2->loadHTMLFile($item['page_url'], LIBXML_ERR_NONE)){ // get page
-					if($dom2->getElementById('image')){ // if not deleted
-						$data = $dom2->getElementById('image')->getAttribute('src');
-						if(!empty($data)){
-							// download image
-							$data=str_replace(array('samples', 'sample_'), array('images', ''), $data);
-							$item['img_url']=$data;
-							if(strrpos($data, "?")>0){
-								$item['img_name']=htmlentities(substr($data, strrpos($data, "/")+1, - (strlen($data)-strrpos($data, "?"))));
-							}else{
-								$item['img_name']=htmlentities(substr($data, strrpos($data, "/")+1));
-							}
-							file_put_contents(__DIR__.'/../mirror/'.$booru_dir.'/img/'.$item['img_name'], file_get_contents($data));
+				$last_insert=$db->get_last_insert();
+				if(!empty($last_insert)){
+					$start=0;
+					// search if last insert image is in list
+					for ($start=0; $start < $flux_rss->length ; $start++) { 
+						if($last_insert['page_url']==$flux_rss->item($start)->nodeValue){
+							break;
 						}
-						// get stats
-						$data = $dom2->getElementById('stats')->getElementsByTagName('li');
-						if(!empty($data)){
-							$item['posted']=substr($data->item(1)->nodeValue, 8, 19);
-							$item['author']=substr($data->item(1)->nodeValue, 31);
-							$item['author_url']=$data->item(1)->getAttribute('href');
-							$a=explode('x',substr($data->item(2)->nodeValue, 6));
-							$item['width']=$a[0];
-							$item['height']=$a[1];
-							$a=$data->item(3)->getElementsByTagName('a');
-							if(!empty($a) && $a->length >0){
-								$item['source']=$a->item(0)->getAttribute('href');
-							}else{
-								$item['source']= substr($data->item(3)->nodeValue, 9);
-							}
-							$item['rating']=substr($data->item(4)->nodeValue, 8);
-							$a=$data->item(5)->getElementsByTagName('span');
-							if(!empty($a) && $a->length >0){
-								$item['score']=$a->item(0)->nodeValue;
-							}else{
-								$item['score']="";
-							}
-							$item['tags']='';
-							$data = $dom2->getElementById('tag-sidebar')->getElementsByTagName('li');
+					}
+					$start--;
+				}else{
+					$start=$flux_rss->length-1;
+				}
+				for($i=$start; $i>2;$i--){
+					$item=array();
+					$item['page_url']=$flux_rss->item($i)->nodeValue;
+					$item['file_id']=substr($item['page_url'], strrpos($item['page_url'], '=')+1);
+					$dom2 = new DOMDocument();
+					if(@$dom2->loadHTMLFile($item['page_url'], LIBXML_ERR_NONE)){ // get page
+						if($dom2->getElementById('image')){ // if not deleted
+							$data = $dom2->getElementById('image')->getAttribute('src');
 							if(!empty($data)){
-								foreach ($data as $tag) {
-									$a=$tag->getElementsByTagName('a');
-									if(!empty($a) && $a->length >0){
-										$item['tags'].=$a->item(1)->nodeValue.";";
+								// download image
+								$data=str_replace(array('samples', 'sample_'), array('images', ''), $data);
+								$item['img_url']=$data;
+								if(strrpos($data, "?")>0){
+									$item['img_name']=htmlentities(substr($data, strrpos($data, "/")+1, - (strlen($data)-strrpos($data, "?"))));
+								}else{
+									$item['img_name']=htmlentities(substr($data, strrpos($data, "/")+1));
+								}
+								file_put_contents(__DIR__.'/../mirror/'.$booru_dir.'/img/'.$item['img_name'], file_get_contents($data));
+							}
+							// get stats
+							$data = $dom2->getElementById('stats')->getElementsByTagName('li');
+							if(!empty($data)){
+								$item['posted']=substr($data->item(1)->nodeValue, 8, 19);
+								$item['author']=substr($data->item(1)->nodeValue, 31);
+								$item['author_url']=$data->item(1)->getAttribute('href');
+								$a=explode('x',substr($data->item(2)->nodeValue, 6));
+								$item['width']=$a[0];
+								$item['height']=$a[1];
+								$a=$data->item(3)->getElementsByTagName('a');
+								if(!empty($a) && $a->length >0){
+									$item['source']=$a->item(0)->getAttribute('href');
+								}else{
+									$item['source']= substr($data->item(3)->nodeValue, 9);
+								}
+								$item['rating']=substr($data->item(4)->nodeValue, 8);
+								$a=$data->item(5)->getElementsByTagName('span');
+								if(!empty($a) && $a->length >0){
+									$item['score']=$a->item(0)->nodeValue;
+								}else{
+									$item['score']="";
+								}
+								$item['tags']='';
+								$data = $dom2->getElementById('tag-sidebar')->getElementsByTagName('li');
+								if(!empty($data)){
+									foreach ($data as $tag) {
+										$a=$tag->getElementsByTagName('a');
+										if(!empty($a) && $a->length >0){
+											$item['tags'].=$a->item(1)->nodeValue.";";
+										}
 									}
 								}
 							}
 						}
 					}
+					$db->insert($item);
 				}
-				$db->insert($item);
-			}
-			if($start>2){
-				echo 'Update done';
+				if($start>2){
+					echo 'Update done';
+				}else{
+					echo 'No news';
+				}
 			}else{
-				echo 'No news';
+				echo 'error';
 			}
 		}
 	}
